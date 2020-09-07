97c2b9fb2f81f301db0accef7b2540116f602c30
Assaf Sapir <assapir@github.com>
2020-09-07 20:12:18 +0300

    Prevernt timeout - smaller repo

--- tests/npm.js
+++ tests/npm.js
@@ -1,39 +1,36 @@
 'use strict'
 
 const npm = require('../lib/npm')
-const { test, beforeEach } = require('tap')
+const { test } = require('tap')
 const Git = require('../lib/git')
 const removeFolderIfExist = require('./utils')
 const fs = require('fs').promises
 const { join, resolve } = require('path')
 
-beforeEach(async () => {
-  const dest = resolve('./tmp')
-  await removeFolderIfExist(dest)
-})
-
 test('npm.install install node_modules to default location', async t => {
   const dest = resolve('./tmp') // that the default path
   await removeFolderIfExist(dest)
   await Git.clone({
-    repository: 'https://github.com/assapir/gh-deploy',
-    branch: 'main',
+    repository: 'https://github.com/npm/ci-detect.git',
+    branch: 'master',
     destination: dest
   })
 
   await npm.install()
   t.resolves(async () => await fs.stat(join(dest, 'node_modules')))
+  await removeFolderIfExist(dest)
 })
 
 test('npm.install install node_modules to specified location', async t => {
   const dest = resolve('./tmp/insider')
   await removeFolderIfExist(dest)
   await Git.clone({
-    repository: 'https://github.com/assapir/gh-deploy',
-    branch: 'main',
+    repository: 'https://github.com/npm/ci-detect.git',
+    branch: 'master',
     destination: dest
   })
 
   await npm.install({ path: dest })
   t.resolves(async () => await fs.stat(join(dest, 'node_modules')))
+  await removeFolderIfExist(dest)
 })
